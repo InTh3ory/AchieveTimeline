d0a2c8a15761fa9f6016f9cdeaf945991a4fc3e1
Ras <ras.ondrasek@gmail.com>
2012-10-28 15:00:42 -0700

    Creating AGRequirements servlet

---
+++ src/com/web/achievetimeline/server/AGRequirements.java
@@ -0,0 +1,35 @@
+package com.web.achievetimeline.server;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+
+@SuppressWarnings("serial")
+public class AGRequirements extends HttpServlet {
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+        String userId = user.getUserId();
+	}
+}
